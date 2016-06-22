d80b70c6207afffabff5d76d71c8779f6674a3a6
think <blankoff@126.com>
2016-06-22 15:16:36 +0800

    数据的静态读取

--- js/orgchart.js
+++ js/orgchart.js
@@ -60,54 +60,199 @@ OrgchartDemo.prototype = {
   }
 };
 
-// var data = [
-//   {"id":1, "pid":0, "logo":"../images/logo/1.jpg", "title": "CEO", "name":"张璟熙", "dept": "总裁办"},
-//   {"id":2, "pid":1, "logo":"../images/logo/2.jpg", "title": "CTO", "name":"张乔斯", "dept": "技术部"}, 
-//   {"id":5, "pid":2, "logo":"../images/logo/4.jpg", "title": "研发工程师1", "name":"张梦琪", "dept": "技术部"}, 
-//   {"id":8, "pid":5, "logo":"../images/logo/3.jpg", "title": "研发工程师2", "name":"张紫萱", "dept": "技术部"},   
-//   {"id":9, "pid":8, "logo":"../images/logo/6.jpg", "title": "研发工程师3", "name":"张三", "dept": "技术部"},    
-//   {"id":6, "pid":2, "logo":"../images/logo/5.jpg", "title": "研发工程师4", "name":"马延行", "dept": "技术部"},   
-//   {"id":11, "pid":6, "logo":"../images/logo/5.jpg", "title": "研发工程师5", "name":"张三", "dept": "技术部"},
-//   {"id":3, "pid":1, "logo":"../images/logo/3.jpg", "title": "CPO", "name":"相札", "dept": "产品部"},
-//   {"id":12, "pid":3, "logo":"../images/logo/7.jpg", "title": "产品经理1", "name":"张湛忻", "dept": "产品部"},
-//   {"id":7, "pid":12, "logo":"../images/logo/9.jpg", "title": "产品经理2", "name":"张妤嫒", "dept": "产品部"}, 
-//   {"id":13, "pid":3, "logo":"../images/logo/8.jpg", "title": "产品经理3", "name":"张和栩", "dept": "产品部"},
-//   {"id":14, "pid":13, "logo":"../images/logo/5.jpg", "title": "产品经理4", "name":"马*泽", "dept": "产品部"},
-//   {"id":15, "pid":13, "logo":"../images/logo/5.jpg", "title": "产品经理5", "name":"马证强", "dept": "产品部"},
-//   {"id":17, "pid":1, "logo":"../images/logo/3.jpg", "title": "COO", "name":"杨蓉倚", "dept": "运营部"},
-//   {"id":22, "pid":17, "logo":"../images/logo/10.jpg", "title": "市场总经理", "name":"杨亮哲", "dept": "运营部"},
-//   {"id":18, "pid":22, "logo":"../images/logo/3.jpg", "title": "市场经理1", "name":"张三", "dept": "运营部"},
-//   {"id":23, "pid":22, "logo":"../images/logo/3.jpg", "title": "市场经理2", "name":"张三", "dept": "运营部"},
-//   {"id":20, "pid":22, "logo":"../images/logo/3.jpg", "title": "市场经理3", "name":"张三", "dept": "运营部"}
-// ];
-function transformData(callback) {
-  $.get('../orgdata.json', function (result) {
-    var data = [];
-    data.push({
-      "id": result.root.data.id,
-      "pid": result.root.data.parent_id,
-      "dept": result.root.data.name,
-      "employees": result.root.data.employees,
-      "leaf": result.root.data.leaf_flag
-    });
-    loopGetData(result.root.childs);
+var data = [
+  {
+    "id": 1,
+    "pid": 0,
+    "logo": "../images/logo/1.jpg",
+    "title": "CEO",
+    "name": "张璟熙",
+    "dept": "总裁办",
+    "employees": 288
+  },
+  {
+    "id": 2,
+    "pid": 1,
+    "logo": "../images/logo/2.jpg",
+    "title": "CTO",
+    "name": "张乔斯",
+    "dept": "技术部",
+    "employees": 28
+  },
+  {
+    "id": 5,
+    "pid": 2,
+    "logo": "../images/logo/4.jpg",
+    "title": "研发工程师1",
+    "name": "张梦琪",
+    "dept": "技术部",
+    "employees": 1
+  },
+  {
+    "id": 8,
+    "pid": 5,
+    "logo": "../images/logo/3.jpg",
+    "title": "研发工程师2",
+    "name": "张紫萱",
+    "dept": "技术部",
+    "employees": 1
+  },
+  {
+    "id": 9,
+    "pid": 8,
+    "logo": "../images/logo/6.jpg",
+    "title": "研发工程师3",
+    "name": "张三",
+    "dept": "技术部",
+    "employees": 1
+  },
+  {
+    "id": 6,
+    "pid": 2,
+    "logo": "../images/logo/5.jpg",
+    "title": "研发工程师4",
+    "name": "马延行",
+    "dept": "技术部",
+    "employees": 1
+  },
+  {
+    "id": 11,
+    "pid": 6,
+    "logo": "../images/logo/5.jpg",
+    "title": "研发工程师5",
+    "name": "张三",
+    "dept": "技术部",
+    "employees": 1
+  },
+  {
+    "id": 3,
+    "pid": 1,
+    "logo": "../images/logo/3.jpg",
+    "title": "CPO",
+    "name": "相札",
+    "dept": "产品部",
+    "employees": 33
+  },
+  {
+    "id": 12,
+    "pid": 3,
+    "logo": "../images/logo/7.jpg",
+    "title": "产品经理1",
+    "name": "张湛忻",
+    "dept": "产品部",
+    "employees": 4
+  },
+  {
+    "id": 7,
+    "pid": 12,
+    "logo": "../images/logo/9.jpg",
+    "title": "产品经理2",
+    "name": "张妤嫒",
+    "dept": "产品部",
+    "employees": 5
+  },
+  {
+    "id": 13,
+    "pid": 3,
+    "logo": "../images/logo/8.jpg",
+    "title": "产品经理3",
+    "name": "张和栩",
+    "dept": "产品部",
+    "employees": 6
+  },
+  {
+    "id": 14,
+    "pid": 13,
+    "logo": "../images/logo/5.jpg",
+    "title": "产品经理4",
+    "name": "马*泽",
+    "dept": "产品部",
+    "employees": 7
+  },
+  {
+    "id": 15,
+    "pid": 13,
+    "logo": "../images/logo/5.jpg",
+    "title": "产品经理5",
+    "name": "马证强",
+    "dept": "产品部",
+    "employees": 9
+  },
+  {
+    "id": 17,
+    "pid": 1,
+    "logo": "../images/logo/3.jpg",
+    "title": "COO",
+    "name": "杨蓉倚",
+    "dept": "运营部",
+    "employees": 15
+  },
+  {
+    "id": 22,
+    "pid": 17,
+    "logo": "../images/logo/10.jpg",
+    "title": "市场总经理",
+    "name": "杨亮哲",
+    "dept": "运营部",
+    "employees": 3
+  },
+  {
+    "id": 18,
+    "pid": 22,
+    "logo": "../images/logo/3.jpg",
+    "title": "市场经理1",
+    "name": "张三",
+    "dept": "运营部",
+    "employees": 5
+  },
+  {
+    "id": 23,
+    "pid": 22,
+    "logo": "../images/logo/3.jpg",
+    "title": "市场经理2",
+    "name": "张三",
+    "dept": "运营部",
+    "employees": 6
+  },
+  {
+    "id": 20,
+    "pid": 22,
+    "logo": "../images/logo/3.jpg",
+    "title": "市场经理3",
+    "name": "张三",
+    "dept": "运营部",
+    "employees": 7
+  }
+];
 
-    function loopGetData(datas) {
-      for (var i = 0; i < datas.length; i++) {
-        data.push({
-          "id": datas[i].data.id,
-          "pid": datas[i].data.parent_id,
-          "dept": datas[i].data.name,
-          "employees": datas[i].data.employees,
-          "leaf": datas[i].data.leaf_flag
-        });
-        if (datas[i].childs && datas[i].childs.length) {
-          loopGetData(datas[i].childs);
-        }
-      }
-    }
-    return callback && callback(data);
-  }, 'json');
+function transformData(callback) {
+  // $.get('../orgdata.json', function (result) {
+  //   var data = [];
+  //   data.push({
+  //     "id": result.root.data.id,
+  //     "pid": result.root.data.parent_id,
+  //     "dept": result.root.data.name,
+  //     "employees": result.root.data.employees,
+  //     "leaf": result.root.data.leaf_flag
+  //   });
+  //   loopGetData(result.root.childs);
+
+  //   function loopGetData(datas) {
+  //     for (var i = 0; i < datas.length; i++) {
+  //       data.push({
+  //         "id": datas[i].data.id,
+  //         "pid": datas[i].data.parent_id,
+  //         "dept": datas[i].data.name,
+  //         "employees": datas[i].data.employees,
+  //         "leaf": datas[i].data.leaf_flag
+  //       });
+  //       if (datas[i].childs && datas[i].childs.length) {
+  //         loopGetData(datas[i].childs);
+  //       }
+  //     }
+  //   }
+  return callback && callback(data);
+  // }, 'json');
 }
 $(function () {
   var demo = new OrgchartDemo();
